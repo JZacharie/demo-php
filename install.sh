e6e2054148434720e1840980b7e75b3af3baa68c
JZacharie <joseph@zacharie.org>
2023-11-23 15:17:24 +0100

    update installer for EC2 with docker

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-echo "Installing runtipi..."
+echo "Installing pimcore..."
 
 ARCHITECTURE="$(uname -m)"
 # Not supported on 32 bits systems
 if [[ "$ARCHITECTURE" == "armv7"* ]] || [[ "$ARCHITECTURE" == "i686" ]] || [[ "$ARCHITECTURE" == "i386" ]]; then
-  echo "runtipi is not supported on 32 bits systems"
+  echo "pimcore is not supported on 32 bits systems"
   exit 1
 fi
 
@@ -164,17 +164,6 @@ function check_dependency_and_install() {
 # Example
 # check_dependency_and_install "openssl"
 
-# If version was not given it will install the latest version
-if [[ "${VERSION}" == "latest" ]]; then
-  LATEST_VERSION=$(curl -sL https://api.github.com/repos/runtipi/runtipi/releases/latest | grep tag_name | cut -d '"' -f4)
-  VERSION="${LATEST_VERSION}"
-fi
-
-ASSET="runtipi-cli-linux-x64"
-if [ "$ARCHITECTURE" == "arm64" ] || [ "$ARCHITECTURE" == "aarch64" ]; then
-  ASSET="runtipi-cli-linux-arm64"
-fi
-
 # Check if git is installed
 if ! command -v git >/dev/null; then
   echo "Git is not installed. Please install git and restart the script."
@@ -184,26 +173,110 @@ fi
 # Create Demo Project
 docker run -u `id -u`:`id -g` --rm -v `pwd`:/var/www/html pimcore/pimcore:php8.2-latest composer create-project pimcore/demo pimcore
 #Copy ENV
-cp int.env ./pimcore/.env
-cp 30* pimcore/
-cp docker-compose.yaml pimcore/
-#Go in Project
-cd pimcore
-#Patch User
-sed -i "s|#user: '1000:1000'|user: '1000:1000'|" docker-compose.yaml
-#Start MicroServices
-docker-compose pull
-docker-compose up -d
-sleep 5
-. .env
-pwd
-#Configure pimcore from ENV
-docker compose exec php vendor/bin/pimcore-install --no-interaction
-docker compose exec php composer require pimcore/data-importer
-docker compose exec php bin/console pimcore:bundle:list
-docker compose exec php bin/console pimcore:search-backend-reindex
-#Configure SMTP
-sed -i 's/#    mailer:/    mailer:/' config/config.yaml
-sed -i 's/#        transports:/        transports:/' config/config.yaml
-sed -i 's/#            main: /            main: /' config/config.yaml
-sed -i "s|main: smtp://user:pass@smtp.example.com:port|main: $PIMCORE_INSTALL_SMTP|" config/config.yaml
+
+cat >./pimcore/.env <<EOF
+APP_ENV=dev
+FOO=BAR
+PIMCORE_INSTALL_ADMIN_PASSWORD=admin
+PIMCORE_INSTALL_ADMIN_USERNAME=admin
+PIMCORE_INSTALL_MYSQL_PASSWORD=pimcore
+PIMCORE_INSTALL_MYSQL_USERNAME=pimcore
+PIMCORE_INSTALL_MYSQL_HOST_SOCKET=db 
+PIMCORE_INSTALL_MYSQL_DATABASE=pimcore
+PIMCORE_INSTALL_INSTALL_BUNDLES=PimcoreDataHubBundle
+PIMCORE_INSTALL_SMTP=smtps://serviceaccountsmtp:changeit@smtp.societe.io:463
+#TRUSTED_PROXIES=127.0.0.0/8,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16
+#TRUSTED_HOSTS='^(localhost|example\.com)$'
+###< symfony/framework-bundle ###
+EOF
+#cp int.env ./pimcore/.env
+
+cat >./pimcore/30-pimcore-php.ini<<EOF
+services:
+    redis:
+        image: redis:alpine
+        command: [ redis-server, --maxmemory, 128mb, --maxmemory-policy, volatile-lru, --save, '""' ]
+
+    db:
+        image: mariadb:10.11
+        working_dir: /application
+        command: [ mysqld, --character-set-server=utf8mb4, --collation-server=utf8mb4_unicode_ci, --innodb-file-per-table=1 ]
+        volumes:
+            - pimcore-demo-database:/var/lib/mysql
+        environment:
+            MYSQL_ROOT_PASSWORD: ROOT
+            MYSQL_DATABASE: pimcore
+            MYSQL_USER: pimcore
+            MYSQL_PASSWORD: pimcore
+    nginx:
+        image: nginx:stable-alpine
+        ports:
+            - "80:80"
+        volumes:
+            - .:/var/www/html:ro
+            - ./.docker/nginx.conf:/etc/nginx/conf.d/default.conf:ro
+        depends_on:
+            - php
+
+    php:
+        user: '1000:1000' # set to your uid:gid
+        image: pimcore/pimcore:php8.2-debug-latest
+        environment:
+            COMPOSER_HOME: /var/www/html
+            PHP_IDE_CONFIG: serverName=localhost
+#            PIMCORE_INSTALL_MYSQL_USERNAME: pimcore
+#            PIMCORE_INSTALL_MYSQL_PASSWORD: pimcore
+#            PIMCORE_INSTALL_ADMIN_USERNAME: admin
+#            PIMCORE_INSTALL_ADMIN_PASSWORD: admin
+        env_file:
+            - .env
+        depends_on:
+            - db
+        volumes:
+            - .:/var/www/html
+            - ./30-pimcore-php.ini:/usr/local/etc/php/conf.d/30-pimcore-php.ini
+
+    supervisord:
+        user: '1000:1000' # set to your uid:gid
+        image: pimcore/pimcore:php8.2-supervisord-latest
+        depends_on:
+            - db
+        volumes:
+            - .:/var/www/html
+            - ./.docker/supervisord.conf:/etc/supervisor/conf.d/pimcore.conf:ro
+
+    chrome:
+        image: browserless/chrome
+
+    gotenberg:
+        image: gotenberg/gotenberg:7
+
+volumes:
+    pimcore-demo-database:
+EOF
+#cp 30* pimcore/
+
+# cp docker-compose.yaml pimcore/
+
+
+
+# #Go in Project
+# cd pimcore
+# #Patch User
+# sed -i "s|#user: '1000:1000'|user: '1000:1000'|" docker-compose.yaml
+# #Start MicroServices
+# docker-compose pull
+# docker-compose up -d
+# sleep 5
+# . .env
+# pwd
+# #Configure pimcore from ENV
+# docker compose exec php vendor/bin/pimcore-install --no-interaction
+# docker compose exec php composer require pimcore/data-importer
+# docker compose exec php bin/console pimcore:bundle:list
+# docker compose exec php bin/console pimcore:search-backend-reindex
+# #Configure SMTP
+# sed -i 's/#    mailer:/    mailer:/' config/config.yaml
+# sed -i 's/#        transports:/        transports:/' config/config.yaml
+# sed -i 's/#            main: /            main: /' config/config.yaml
+# sed -i "s|main: smtp://user:pass@smtp.example.com:port|main: $PIMCORE_INSTALL_SMTP|" config/config.yaml
